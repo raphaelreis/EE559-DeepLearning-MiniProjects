252c38d0868e3d6f0ba79e9d2227be4b23bcb2d1
bernaert <vincent.bernaert@epfl.ch>
2020-04-12 16:10:17 +0200

    added sigmoid activation

--- Proj1/models/vince_models.py
+++ Proj1/models/vince_models.py
@@ -8,21 +8,20 @@ from torch.nn import functional as F
 from torch import optim
 
 
+# convolutional network for binary classification (1 output unit)
 
 
-class Net2(nn.Module):
+
+class Net2B(nn.Module):
 
     def __init__(self, nb_hidden):
-        super(Net2, self).__init__()
+        super(Net2B, self).__init__()
         
         # number of input channels is 2.
         self.conv1 = nn.Conv2d(2, 32, kernel_size = 5)
         self.conv2 = nn.Conv2d(32, 64, kernel_size = 3)
         self.fc1 = nn.Linear(256, nb_hidden)
         self.fc2 = nn.Linear(nb_hidden, 1)
-        
-        
-        
         
     def forward(self,x):
     
