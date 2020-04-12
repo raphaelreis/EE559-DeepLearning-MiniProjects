03d87c15800c02bd3ea8803b866e9550096830da
bernaert <vincent.bernaert@epfl.ch>
2020-04-12 19:16:16 +0200

    update analysis

--- Proj1/models/vince_models.py
+++ Proj1/models/vince_models.py
@@ -11,17 +11,17 @@ from torch import optim
 # convolutional network for binary classification (1 output unit)
 
 
-
-class Net2B(nn.Module):
+class Net2(nn.Module):
 
     def __init__(self, nb_hidden):
-        super(Net2B, self).__init__()
+        super(Net2, self).__init__()
         
         # number of input channels is 2.
         self.conv1 = nn.Conv2d(2, 32, kernel_size = 5)
         self.conv2 = nn.Conv2d(32, 64, kernel_size = 3)
         self.fc1 = nn.Linear(256, nb_hidden)
-        self.fc2 = nn.Linear(nb_hidden, 1)
+        self.fc2 = nn.Linear(nb_hidden, 1) # single output
+        self.sigmoid = nn.Sigmoid()  
         
     def forward(self,x):
     
@@ -29,15 +29,8 @@ class Net2B(nn.Module):
         x = F.relu(F.max_pool2d(self.conv2(x), kernel_size = 3, stride = 3))
         x = F.relu(self.fc1(x.view(-1, 256)))
         x = self.fc2(x)
-        return x
-
-
-
-
-
-
-
-
+        x = self.sigmoid(x) 
+        return x # returns a probability 
 
 
 
