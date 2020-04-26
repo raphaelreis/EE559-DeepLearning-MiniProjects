e9fc9c5fbebdac5049283842227218061bc3ca4e
bernaert <vincent.bernaert@epfl.ch>
2020-04-26 18:42:16 +0200

    add DataLoader

--- Proj1/utils/loader.py
+++ Proj1/utils/loader.py
@@ -1,4 +1,8 @@
+import torch
+import numpy
 import utils.dlc_practical_prologue as prologue
+from torch.utils.data import Dataset, DataLoader
+
 
 
 
@@ -13,3 +17,27 @@ def load():
     '''
     return prologue.generate_pair_sets(1000)
 
+
+class PairSetMNIST(Dataset):
+    
+    """
+    Generate train input, labels, and classes set from load()
+    
+    """
+    
+    def __init__(self):
+        
+        train_input, train_target, train_classes, test_input, test_target,test_classes=load()
+    
+        self.len = train_input.shape[0]
+        self.x_train = train_input.sub(train_input.mean()).div(train_input.std())
+        self.y_train = train_target
+        self.tr_classes = train_classes
+        
+    def __getitem__(self, index):
+        
+        return self.x_train[index], self.y_train[index]
+
+    def __len__(self):
+
+        return self.len 
