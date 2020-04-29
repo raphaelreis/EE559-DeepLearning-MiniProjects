cc2b036af8d1df777d4894341966c7e9600ce6de
Raphaël Reis Nunes <raphael.reisnunes@epfl.ch>
2020-04-29 11:19:43 +0200

    add sigmoid last layer activation

--- Proj2/neuralnetworks/mlp.py
+++ Proj2/neuralnetworks/mlp.py
@@ -3,7 +3,7 @@ import logging
 
 from .base import Module
 from .feedforward import Feedforward
-from .functions import tanh, MSE, MSE_prime
+from .functions import tanh, MSE, MSE_prime, sigmoid
 # from ..optimizer.sgd import SGD
 
 
@@ -17,7 +17,8 @@ log = logging.getLogger("TestMLP")
 
 class MLP(Module):
     def __init__(self, feature_size, output_size, hidden_size=25, optim='SGD',
-                 lr=0.01, activation='relu', loss='MSE'):
+                 lr=0.01, activation='relu', last_layer_activation='tanh',
+                 loss='MSE'):
         super(MLP, self).__init__()
 
         def parse_loss(loss):
@@ -26,6 +27,14 @@ class MLP(Module):
             else:
                 raise Exception("Not implemented loss function")
 
+        def parse_last_layer_activation(last_layer_activation):
+            if last_layer_activation == 'sigmoid':
+                return sigmoid
+            elif last_layer_activation == 'tanh':
+                return tanh
+            else:
+                raise Exception("Not implemented activation function")
+
         self.lr = lr
         self.optimizer = optim
         self.loss, self.dLoss = parse_loss(loss)
@@ -33,13 +42,15 @@ class MLP(Module):
                                    hidden_size, activation=activation)
         self.linear2 = Feedforward(hidden_size,
                                    output_size, activation=activation)
-
+        self.last_layer_activation = parse_last_layer_activation(
+                                        last_layer_activation)
+    
     def forward(self, X: Tensor):
         self.linear1.forward(X)
         x = self.linear1.Z
         self.linear2.forward(x)
-        log.debug("linear2.Z: {}".format(self.linear2.Z))
-        self.output = tanh(self.linear2.Z)
+        # log.debug("linear2.Z: {}".format(self.linear2.Z))
+        self.output = self.last_layer_activation(self.linear2.Z)
 
     def backward(self, dA: Tensor):
         self.linear2.backward(self.linear2.input, dA)
@@ -58,29 +69,28 @@ class MLP(Module):
 
     def train(self, X, y, epochs=10):
         history_loss = []
-
         if self.optimizer == 'SGD':
             for e in range(epochs):
+                correct = 0
                 for (dat, tar) in zip(X, y):
                     self.forward(dat)
                     loss = self.loss(self.output, tar)
-                    history_loss.append(loss)
+                    history_loss.append(loss.item())
 
-                    log.debug("self.output: {}".format(self.output))
-                    log.debug("tar: {}".format(tar))
-                    log.debug("loss: {}".format(loss))
+                    # log.debug("self.output: {}".format(self.output))
+                    # log.debug("tar: {}".format(tar))
+                    # log.debug("loss: {}".format(loss))
 
                     dL = self.dLoss(self.output, tar)
                     self.backward(dL)
-                    # log.debug("W shape: \n{}"
-                    #           .format(self.linear2.W))
+
                     for idx, ((W, dW), (b, db)) in enumerate(self.get_param()):
                         W_t_1 = (W - self.lr * dW).clone()
                         b_t_1 = (b - self.lr * db).clone()
-                        # log.debug("W_t_1: {}".format(W_t_1))
                         self.set_param_to_layer(idx, W_t_1, b_t_1)
                     
-                    # log.debug("self.linear2.W: {}".format(self.linear2.W))
+                    self
+                    
         else:
             raise NotImplementedError("There is currently no other optimizer")
 
