33b0f637c978b2092e7bc4fcd49874d3203b7333
Sebemery <sebastien.emery@epfl.ch>
2020-05-22 15:51:51 +0200

    figure added

--- Proj1_Seb_/test.py
+++ Proj1_Seb_/test.py
@@ -1,11 +1,18 @@
-from utils.loader import load
+from models import *
+from utils import *
 
 
 
 if __name__ == "__main__":
     
-    data = load()
-    print(data)
-    # model
-    # train 
-    # evaluation 
+    # parse args
+    args = args_parser(description='EE-559 project 1 executable')
+    
+    #args.add_argument('--', default=False, help='Use the full set, can take ages (default False)')
+    
+    Nets_default = Nets()
+    seeds = [1,2,3,4,5,6,7,8,9,10]
+    
+    
+    train_results, test_lossestest_accuracies = evaluate_model(Nets_default.LeNet_sharing_aux, seeds , plot =False, 
+                                                               rotate = True,translate=True,swap_channel = True)
